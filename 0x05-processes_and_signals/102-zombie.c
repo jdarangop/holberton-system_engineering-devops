30ff647ecab6fd34d353eb0a6487d0e41ff511b0
jdarangop <juandiegoap33@gmail.com>
2019-11-05 00:48:07 +0000

    Feature: add task 10 and 11

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * infinite_while - Inifinite loop.
+ * Return: Always 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Build zombie process.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	pid_t child;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		child = fork();
+		printf("Zombie process created, PID: %d\n", child);
+	}
+	infinite_while();
+	return (0);
+}
