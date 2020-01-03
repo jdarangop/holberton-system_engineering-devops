65fda060a6f2d78c5fe01554cead3d540b5416d1
jdarangop <juandiegoap33@gmail.com>
2020-01-03 03:30:11 +0000

    Feature: add others process to puppet file

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -4,6 +4,30 @@ package { 'nginx':
   provider => 'gem',
 }
 
+file { 'holberton school':
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Holberton School',
+}
+
+file_line { 'Set rewrite':
+  path    => '/etc/nginx/sites-available/default',
+  match   => 's/^server {$',
+  line    => 'server {\n\trewrite ^\/redirect_me https:\/\/www.google.com\/ permanent;',
+  replace => 'true',
+}
+
+file { 'Display 404-no-found':
+  path    => '/var/www/html/404_error.html',
+  content => 'Ceci n\'est pas une page',
+}
+
+file_line { 'Set 404-no-found':
+  path    => '/etc/nginx/sites-available/default',
+  match   => 's/^server {$',
+  line    => 'server {\n\terror_page 404 \/404_error.html;\n\tlocation = \/404_error.html {\n\t\troot \/var\/www\/html;\n\t\tinternal;\n\t}',
+  replace => 'true',
+}
+
 file_line { 'Set header':
   path    => '/etc/nginx/sites-available/default',
   match   => 'location \/ {',
