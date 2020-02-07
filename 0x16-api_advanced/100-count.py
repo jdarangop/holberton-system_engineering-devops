d183c86bcfef3e67b44bbd6edabe1d922e904321
jdarangop <juandiegoap33@gmail.com>
2020-02-07 03:02:59 +0000

    Feature: add task 3 to the folder

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" module 100 """
+from requests import get
+import re
+
+
+def count_words(subreddit, word_list, page=None, amount={}):
+    """ top_ten - Returns the titles of the first 10 hot post
+        in a subreddit.
+        parameters: subreddit.
+        Return: the numeber of subscribers or 0 is not a valid subreddit.
+    """
+
+    if page is None:
+        request = get('https://www.reddit.com/r/{}/hot.json'
+                      .format(subreddit), headers={'User-Agent': 'jdarangop'},
+                      allow_redirects=False)
+    else:
+        request = get('https://www.reddit.com/r/{}/hot.json?after={}'
+                      .format(subreddit, page),
+                      headers={'User-Agent': 'jdarangop'},
+                      allow_redirects=False)
+
+    if request.status_code != 200:
+        print()
+    else:
+        after = request.json().get('data').get('after')
+        list_post = request.json().get('data').get('children')
+        for i in list_post:
+            title = i.get('data').get('title')
+            for j in word_list:
+                if amount.get(j) is None:
+                    amount[j] = 0
+                patterns = re.findall("{} |{}$".format(j, j), title)
+                if patterns != []:
+                    amount[j] += len(patterns)
+        if after:
+            count_words(subreddit, word_list, after, amount)
+        else:
+            for key, value in amount.items():
+                if value != 0:
+                    print("{}: {}".format(key, value))
