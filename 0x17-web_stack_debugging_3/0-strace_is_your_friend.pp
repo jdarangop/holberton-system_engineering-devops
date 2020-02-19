0135157db2c23670b102b3f4c461c31ad021682b
jdarangop <juandiegoap33@gmail.com>
2020-02-19 17:40:51 +0000

    Fix: change path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # puppet to fix bug
 
 exec { 'Replace String':
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  command => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
 }
