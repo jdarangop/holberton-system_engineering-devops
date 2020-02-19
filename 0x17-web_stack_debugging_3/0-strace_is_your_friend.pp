8aa7ebd12ab40f28426150cdbb9be14d3d425c11
jdarangop <juandiegoap33@gmail.com>
2020-02-19 17:38:42 +0000

    Fix: change path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # puppet to fix bug
 
 exec { 'Replace String':
-  command => 'sed -i "s/phpp/php/g"',
-  path    => '/var/www/html/wp-settings.php',
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
 }
