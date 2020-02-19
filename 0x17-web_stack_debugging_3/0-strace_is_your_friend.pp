1b3f684dd5c21bb3c2249f91c3ff00dba37cec1d
jdarangop <juandiegoap33@gmail.com>
2020-02-19 19:15:46 +0000

    Fix: add path to sed command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # puppet to fix bug
 
 exec { 'Replace String':
-  command => 'sudo sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
+  command => '/bin/sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
 }
