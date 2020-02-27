53f2da7c4fdbb707b9e6ff2aa14c82adbd42d61b
jdarangop <juandiegoap33@gmail.com>
2020-02-27 20:19:29 +0000

    Fix: add mandate to restart the service

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,8 @@
 # Change to allow more request
 
-exec { 'restart-nginx':
-  command  => '/usr/bin/sudo /etc/init.d/nginx restart',
-}
-
 exec { 'fix--for-nginx':
   command  => '/usr/bin/sudo /bin/sed -i "s/ULIMIT=\"\-n [0-9]*\"/ULIMIT=\"\-n 4096\"/g" /etc/default/nginx',
+}
+->exec { 'restart-nginx':
+  command  => '/usr/bin/sudo /etc/init.d/nginx restart',
 }
